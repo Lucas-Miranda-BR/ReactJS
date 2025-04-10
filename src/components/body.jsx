f2822971745a0d4a8b9d13b306df02170119752e
Lucas <lucasmirandabr0508@gmail.com>
2025-04-10 17:49:12 +0000

    Introdução de react em JSX

---
+++ src/components/body.jsx
@@ -0,0 +1,7 @@
+export default function Body() {
+    return (
+        <div>
+            <h1>Rain World</h1>
+        </div>
+    )
+}
---
+++ src/components/galery.jsx
@@ -0,0 +1,16 @@
+export default function Galery() {
+    return (
+        <div className="card" style={{ width: "18rem" }}>
+            <img src="https://static.wikitide.net/rainworldwiki/thumb/4/43/Downpour_gallery_icon.png/100px-Downpour_gallery_icon.png" className="card-img-top" alt="Downpour Expansion" />
+            <div className="card-body">
+                <h5 className="card-title">Downpour Slugcats.</h5>
+                <p className="card-text">
+                    Arte demonstrado os slugcats dentro da expansão "Downpour" de "Rain World".
+                </p>
+                <a href="#" className="btn btn-primary">
+                    Descubra mais.
+                </a>
+            </div>
+        </div>
+    )
+}
